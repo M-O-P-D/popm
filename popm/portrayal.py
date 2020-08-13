b023bd17e74f5be761b90b5a0efbfbf685589132
Andrew Smith <andrew@friarswood.net>
2020-08-13 15:16:47 +0100

    add distance matrix. closes #2

--- popm/portrayal.py
+++ popm/portrayal.py
@@ -1,9 +1,12 @@
 from .agents import *
 
+# Currently only color and radius properties are supported, see
+# mesa-geo/mesa_geo/visualization/templates/js/LeafletMap.js 
+
 def portray_map(agent):
   if isinstance(agent, ForceAreaAgent):
     return { "color": "Blue" }
   elif isinstance(agent, ForceCentroidAgent):
     return { "radius": "2", "color": "Red" }
   else:
-    return {}
+    raise TypeError("don't know how to portray object of type %s" % type(agent))
