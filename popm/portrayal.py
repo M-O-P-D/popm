9de125ff89af57dd12901db7f15a3ec6514a8977
Andrew Smith <andrew@friarswood.net>
2020-08-20 15:44:32 +0100

    work on #5. still need to aggregate populations and rename area

--- popm/portrayal.py
+++ popm/portrayal.py
@@ -1,4 +1,4 @@
-from .agents import ForceAreaAgent, ForceCentroidAgent, ForcePSUAgent
+#from .agents import ForceAreaAgent, ForceCentroidAgent, ForcePSUAgent
 
 # Currently only color and radius properties are supported, see
 # mesa-geo/mesa_geo/visualization/templates/js/LeafletMap.js
