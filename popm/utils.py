371687250c9157339aaea719d7762f3659edc440
Andrew Smith <andrew@friarswood.net>
2020-09-17 14:58:01 +0100

    add isochrone example code #24

--- popm/utils.py
+++ popm/utils.py
@@ -5,6 +5,11 @@ from shapely.geometry import mapping, shape
 from shapely.ops import transform
 import shapely.wkt
 
+_bng_proj = pyproj.Proj(init='epsg:27700')
+_lonlat_proj = pyproj.Proj(init='epsg:4326')
+_proj_bng2lonlat = pyproj.Transformer.from_proj(_bng_proj, _lonlat_proj)
+_proj_lonlat2bng = pyproj.Transformer.from_proj(_lonlat_proj, _bng_proj)
+
 def serialise_geometry(geom):
   return geom.wkt
 
@@ -12,10 +17,8 @@ def deserialise_geometry(wkt):
   return shapely.wkt.loads(wkt)
 
 def bng2lonlat(shape):
-  project = pyproj.Transformer.from_proj(
-      pyproj.Proj(init='epsg:27700'), # source coordinate system
-      pyproj.Proj(init='epsg:4326')) # destination coordinate system
+  return transform(_proj_bng2lonlat.transform, shape)  
 
-  return transform(project.transform, shape)  # apply projection
-
+def lonlat2bng(shape):
+  return transform(_proj_lonlat2bng.transform, shape)  
 
