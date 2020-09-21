0916e31dc6865222b450cb17db70bf4b6aa472c1
Andrew Smith <andrew@friarswood.net>
2020-09-21 13:56:50 +0100

    get plottable route from graphhopper

--- travel.py
+++ travel.py
@@ -25,6 +25,8 @@ query = [
   ("point", "53.9233771,-1.8003073"),
   ("vehicle", "car"),
   ("calc_points", "true"),
+  ("points_encoded", "false"), # means points are just lon/lat, which is what we want
+  ("details", "max_speed")
 ]
 
 response = requests.get(url(endpoint, query))
@@ -41,32 +43,43 @@ else:
     print("%.2fkm in %.2fh" % (dist, time))
 
 # %%
+import numpy as np
+from matplotlib import pyplot as plt
 
-body = json.dumps({
-  "from_points": [
-    [-1.8270279, 53.9261829],
-    [-1.8003073, 53.9233771],
-    [-1.5540781,53.8039043]
-  ],
-  "to_points": [
-    [-1.8270279, 53.9261829],
-    [-1.8003073, 53.9233771],
-    [-1.5540781,53.8039043]
-  ],
-  "vehicle": "car",
-  "elevation": False,
-  "calc_points": False,
-  "out_arrays": ["distances", "times"]
-})
-#curl -X POST -H "Content-Type: application/json" "https://graphhopper.com/api/1/route?key=[YOUR_KEY]" -d '{"elevation":false,"points":[[-0.087891,51.534377],[-0.090637,51.467697]],"vehicle":"car"}'
+route = result["paths"][0]
+bbox = [[route["bbox"][1], route["bbox"][0]], [route["bbox"][3], route["bbox"][2]]] 
+points = np.array([[p[1],p[0]] for p in route["points"]["coordinates"]])
+#legs = [(node["time"]/1000.0, node["interval"]) for node in route["instructions"]]
+#points seems to be roughtly evenly space in time, i.e. further apart on faster roads
+# so just linspace the end time (rather than each leg)
 
-response = requests.post(url("matrix", []), body, headers={"Content-Type": "application/json"})
+times = np.linspace(0, result["paths"][0]["time"], len(points))
 
-if not response.ok:
-  print(response)
-else:
-  result = response.json()
-  dists = np.array(result["distances"]) / 1000.0 # km
-  times = np.array(result["times"]) / 3600.0 # h
-  print(dists)
-  print(times)
+plt.plot(times, points[:,0])
+
+plt.plot(times, points[:,1])
+
+# %%
+import folium
+from folium.plugins import MarkerCluster
+import pandas as pd
+
+map=folium.Map(location=[53.9261829,-1.8270279], zoom_start=12)
+map.fit_bounds(bbox) # doesnt work
+map
+# %%
+
+for i, p in enumerate(points):
+  folium.CircleMarker(p, popup=str(i)).add_to(map) 
+
+map
+# %%
+m = folium.CircleMarker(points[0], radius=20, popup="movable?")
+
+m.add_to(map)
+# %%
+l = folium.PolyLine(points)
+l.add_to(map)nazi 
+map
+
+# %%
