df708293cebdb6be96257a915380ded87df302bc
Andrew Smith <andrew@friarswood.net>
2020-09-24 10:13:01 +0100

    switch to BNG coords

--- cached_routes.py
+++ cached_routes.py
@@ -6,18 +6,14 @@ import pandas as pd
 df = pd.read_csv("data/force_centroid_routes.zip") 
 
 print(df.head())
-print(df.tail())
 # %%
 from shapely import wkt
 df["geometry"] = df["geometry"].apply(wkt.loads)
 
 print(df.head())
-print(df.tail())
 
 # %%
 gdf = gpd.GeoDataFrame(df)
 
 gdf.head()
-# %%
-gdf.plot()
-# %%
+
