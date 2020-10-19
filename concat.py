a620b6e6f5a1d654f9fa09827d7b7c7d874045b3
Andrew Smith <andrew@friarswood.net>
2020-10-19 14:52:41 +0100

    improve collation script

--- concat.py
+++ concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os
+from pathlib import Path
 import argparse
 import glob
 import pandas as pd
@@ -11,16 +11,18 @@ def run(config):
 
   tables = {"[0-9]": "", "_locations": "_locations", "_allocations": "_allocations", "_resources": "_resources" }
 
-  os.mkdir("scenario/%s" % config)  
+  Path("scenario/%s" % config).mkdir(exist_ok=True)
   shutil.copyfile("scenario/%s.json" % config, "scenario/%s/%s.json" % (config, config))
 
   for t in tables.keys():
     pattern = "scenario/%s%s*.csv" % (config, t)
     files = glob.glob(pattern)
+    if not files:
+      raise ValueError("no files found matching '%s'" % pattern)
     df = pd.DataFrame()
     for f in files:
       df = df.append(pd.read_csv(f), ignore_index=True)
-      #os.remove(f)
+      Path(f).unlink() # delete file
     print(df.head(), len(df))
     # saving as compressed file is broken
     df.to_csv("scenario/%s/%s%s.csv" % (config, config, tables[t]), index=False)
