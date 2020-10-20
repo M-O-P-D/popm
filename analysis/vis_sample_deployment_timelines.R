f47b8baf6ead33ee11ab556eafaff4fa8ed7dd65
Dan Birks <danbirks@gmail.com>
2020-10-20 22:41:51 +0100

    Adding basic R visualisation scripts for model outputs

---
+++ analysis/vis_sample_deployment_timelines.R
@@ -0,0 +1,88 @@
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(tidyverse)
+library(gridExtra)
+library(reshape2)
+library(rstudioapi)  
+
+#USER PARAM
+n_events <- 10
+
+for(num_events in 1:n_events) 
+{
+  #Load data 
+  setwd(paste0("../", num_events, " Events"))
+  
+  df <- read_csv(paste0(num_events,"events.csv"))
+  df$Event <- as_factor(df$Event)
+  dflookup <- read_csv(paste0(num_events,"events_locations.csv"))
+  head(df)
+  head(dflookup)
+  
+  df <- merge(df, dflookup, by = "RunId")
+  head(df)
+  
+  small_immediate_start <- filter(df, EventStart == 0 & Required == 500)
+  medium_immediate_start <- filter(df, EventStart == 0 & Required == 2000)
+  large_immediate_start <- filter(df, EventStart == 0 & Required == 5000)
+  
+  #store var for number of rows that need to be extracted to generate 12 sample plots 
+  sample_count_rows <- num_events*12*24 #number of events x 12 plots x 24 hours 
+  
+  large_immediate_start_sample <- large_immediate_start[1:sample_count_rows,]
+  medium_immediate_start_sample <- medium_immediate_start[1:sample_count_rows,]
+  small_immediate_start_sample <- small_immediate_start[1:sample_count_rows,]
+  tail(small_immediate_start_sample)
+  
+  #With lookup for event id string
+  head(large_immediate_start_sample)
+  
+  p1 <- ggplot(data = large_immediate_start_sample, aes(x=Time, y=DeployedPct)) +
+    xlim("1", "2", "3", "4", "5", "6","7", "8", "9","10", "11", "12","13", "14", "15","16", "17", "18","19", "20", "21", "22", "23") +
+    geom_line(aes(color = Event)) + 
+    #geom_point(size = 2, aes(color = as.factor(Event), shape = as.factor(Event))) +
+    geom_segment(size=2, aes(x = 1, y = 0, xend = 1, yend = 10), color="grey") +
+    geom_segment(size=2, aes(x = 4, y = 0, xend = 4, yend = 40), color="grey") +
+    geom_segment(size=2, aes(x = 8, y = 0, xend = 8, yend = 60), color="grey") +
+    labs(title = paste("Sample of" , num_events , "simultaneous large (200 PSUs) seats-of-unrest scenarios"),
+         subtitle = "Color Lines: % of required resources deployed to event, Vertical Bars: Recognised mobilisation targets",
+         y = "% of required resources", x = "Hours from event start")+ 
+    scale_fill_brewer(palette="Dark2") +
+    facet_wrap(~ EventLocations , ncol=2)
+  
+  
+  p2 <- ggplot(data = medium_immediate_start_sample, aes(x=Time, y=DeployedPct)) +
+    xlim("1", "2", "3", "4", "5", "6","7", "8", "9","10", "11", "12","13", "14", "15","16", "17", "18","19", "20", "21", "22", "23") +
+    geom_line(aes(color = Event)) + 
+    #geom_point(size = 2, aes(color = as.factor(Event), shape = as.factor(Event))) +
+    geom_segment(size=2, aes(x = 1, y = 0, xend = 1, yend = 10), color="grey") +
+    geom_segment(size=2, aes(x = 4, y = 0, xend = 4, yend = 40), color="grey") +
+    geom_segment(size=2, aes(x = 8, y = 0, xend = 8, yend = 60), color="grey") +
+    labs(title = paste("Sample of" , num_events , "simultaneous medium (80 PSUs) seats-of-unrest scenarios"),
+         subtitle = "Color Lines: % of required resources deployed to event, Vertical Bars: Recognised mobilisation targets",
+         y = "% of required resources", x = "Hours from event start")+ 
+    scale_fill_brewer(palette="Dark2") +
+    facet_wrap(~ EventLocations , ncol=2)
+  
+  p3 <- ggplot(data = small_immediate_start_sample, aes(x=Time, y=DeployedPct)) +
+    xlim("1", "2", "3", "4", "5", "6","7", "8", "9","10", "11", "12","13", "14", "15","16", "17", "18","19", "20", "21", "22", "23") +
+    geom_line(aes(color = Event)) +
+    #geom_point(size = 2, aes(color = as.factor(Event), shape = as.factor(Event))) +
+    geom_segment(size=2, aes(x = 1, y = 0, xend = 1, yend = 10), color="grey") +
+    geom_segment(size=2, aes(x = 4, y = 0, xend = 4, yend = 40), color="grey") +
+    geom_segment(size=2, aes(x = 8, y = 0, xend = 8, yend = 60), color="grey") +
+    labs(title = paste("Sample of" , num_events , "simultaneous small (20 PSUs) seats-of-unrest scenarios"),
+         subtitle = "Color Lines: % of required resources deployed to event, Vertical Bars: Recognised mobilisation targets",
+         y = "% of required resources", x = "Hours from event start", fill = "Dose (mg)")+ 
+    facet_wrap(~ EventLocations , ncol=2)
+  
+  
+  pdf(paste0("Sample_deployment_timelines_",num_events,"_med-large_events.pdf"), height = 11, width = 11)
+  grid.arrange(p1,p2)
+  dev.off()
+  
+  pdf(paste0("Sample_deployment_timelines_",num_events,"_small-medium_events.pdf"), height = 11, width = 11)
+  grid.arrange(p2,p3)
+  dev.off()
+}
---
+++ analysis/vis_systemwide_performance_by_event_PFA.R
@@ -0,0 +1,63 @@
+#----------------------------------------------------------------------------------------------------------------------
+#Look at overall performance - BOX PLOTS
+#- Boxplots represent % of Required Allocation at each simulated event in police force area  
+#----------------------------------------------------------------------------------------------------------------------
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(tidyverse)
+library(gridExtra)
+library(reshape2)
+library(rstudioapi)  
+
+#USER PARAM
+n_events <- 10
+
+results <- data.frame(Event=factor(),
+                       AllocatedPct=double(),
+                       NumEvents=integer(),
+                       EventSize=factor(), 
+                       stringsAsFactors=FALSE) 
+
+for(num_events in 1:n_events) 
+{
+
+  #Load data 
+  setwd(paste0("../", num_events, " Events"))
+  raw <- read_csv(paste0(num_events,"events.csv"))
+  raw$Event <- as_factor(raw$Event)
+  
+  head(raw)
+  
+  #Set USER PARAM
+  for(event_size in c("small","medium","large")) 
+  {
+    print(paste("Generating outcomes for", num_events, event_size, "events" ))
+    
+    #Subset data based on Param - pull only immediate events of right size - only extract allocation at hour 23
+    if(event_size == "small") {df <- filter(raw, EventStart == 0 & Time == 23 & Required == 500)}
+    if(event_size == "medium") {df <- filter(raw, EventStart == 0 & Time == 23 & Required == 2000)}
+    if(event_size == "large") {df <- filter(raw, EventStart == 0 & Time == 23 & Required == 5000)}
+    
+    #drop everything apart from RunId and AllocatedPCT
+    df <- df[,c('Event','AllocatedPct')]
+    
+    df$num_events = num_events
+    df$event_size = event_size
+    
+    results <- rbind(results, df)
+  }
+}
+
+p1 <- ggplot(results, aes(x=factor(num_events), y=AllocatedPct, fill=factor(event_size))) + 
+  geom_boxplot() +
+  labs(title = paste("Allocation Trends by size and number of seats-of-unrest scenarios"),
+       subtitle = "Boxplots represent % of Required Allocation at each simulated event in police force area. 1 to 3 events all combinations 4+ Events - sample of 10000 combinations .",
+       y = "% of Required Allocation at Event", x = "Number of simultaneous events",
+       caption = paste("\n\nGenerating Script:", print(rstudioapi::getActiveDocumentContext()$path))) +
+  facet_wrap(~Event)
+
+
+pdf(paste0("../XBoxplots_by_PFA_size_number_events.pdf"), height = 11, width = 18)
+p1
+dev.off()
