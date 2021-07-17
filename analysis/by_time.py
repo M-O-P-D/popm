fdacc8ac79501ba57096cd381882ef0c7ea40387
virgesmith <andrew@friarswood.net>
2021-07-17 08:51:41 +0100

    run on new output

--- analysis/by_time.py
+++ analysis/by_time.py
@@ -17,50 +17,51 @@ forces = ['Avon and Somerset', 'Beds Cambs Herts', 'Cheshire',
 
 def analysis(scenario):
   path = Path(f"./model-output/{scenario}")
-  allocations = pd.read_csv(path / "allocations.csv", index_col=["RunId", "EventForce", "AssignedForce"])
-  deployments = pd.read_csv(path / "deployments.csv")[["RunId", "Event", "Hit10Pct", "Hit40Pct", "Hit60Pct", "Hit100Pct"]].drop_duplicates().set_index(["RunId", "Event"])
-  locations = pd.read_csv(path / "locations.csv", index_col="RunId")
-
-  # check we havent lost any data
-  #assert len(deployments.index.get_level_values(0).unique()) == 41*40/2 #10000
-  assert len(deployments.index.get_level_values(1).unique()) == 41
-  #assert len(deployments) == 70000 #41*40
-
-  print(allocations)
+  #allocations = pd.read_csv(path / "acitve_psus.csv", index_col=["RunId", "EventForce", "AssignedForce"])
+  deployments = pd.read_csv(path / "deployment_times.csv").set_index(["RunId", "location", "requirement_frac"])#.unstack(level=[1,2]) #.unstack(level=2)
+  #print(allocations)
+  #deployments = deployments[deployments.index.get_level_values("location") == "Greater Manchester"]
+  #deployments.to_csv("./gm.csv")
   print(deployments)
-  print(locations)
-  #print(locations.columns.values)
 
-  # appearances = {force: len(locations[locations.EventLocations.str.contains(force)]) for force in forces}
+  means = deployments["actual"].unstack(level=[1,2]).mean().unstack(level=1).add_prefix("mean-")
+  print(means)
 
-  appearances = pd.DataFrame(data={"Force": forces})
-  appearances["Appearances"] = appearances.Force.apply(lambda force: len(locations[locations.EventLocations.str.contains(force)]))
-  appearances = appearances.drop_duplicates() \
-                           .set_index("Force") \
-                           .sort_values(by="Appearances")
+  stddevs = deployments["actual"].unstack(level=[1,2]).std().unstack(level=1).add_prefix("stddev-")
+  print(stddevs)
 
-  print(appearances.to_string())
-  print(appearances.mean())
+  means.merge(stddevs, left_index=True, right_index=True).to_csv(f"{scenario}_summary.csv")
 
-  # subtract mobilisation times
-  deployments.Hit10Pct -= 1.0
-  deployments.Hit40Pct -= 4.0
-  deployments.Hit60Pct -= 8.0
-  deployments.Hit100Pct -= 16.0
+  # # appearances = {force: len(locations[locations.EventLocations.str.contains(force)]) for force in forces}
 
-  print(deployments)
-  # need to drop metric as it persists as an index level
-  means = pd.DataFrame(data={ metric: deployments[[metric]].unstack(level=1).mean().droplevel(0) for metric in ["Hit10Pct", "Hit40Pct", "Hit60Pct", "Hit100Pct"]}) \
-    .add_prefix("mean-")
+  # appearances = pd.DataFrame(data={"Force": forces})
+  # appearances["Appearances"] = appearances.Force.apply(lambda force: len(locations[locations.EventLocations.str.contains(force)]))
+  # appearances = appearances.drop_duplicates() \
+  #                          .set_index("Force") \
+  #                          .sort_values(by="Appearances")
 
-  stddevs = pd.DataFrame(data={ metric: deployments[[metric]].unstack(level=1).std().droplevel(0) for metric in ["Hit10Pct", "Hit40Pct", "Hit60Pct", "Hit100Pct"]}) \
-    .add_prefix("stddev-")
+  # print(appearances.to_string())
+  # print(appearances.mean())
 
-  dep_times = means.merge(stddevs, left_index=True, right_index=True) 
+  # # subtract mobilisation times
+  # deployments.Hit10Pct -= 1.0
+  # deployments.Hit40Pct -= 4.0
+  # deployments.Hit60Pct -= 8.0
+  # deployments.Hit100Pct -= 16.0
 
-  print(dep_times)
-  assert len(dep_times) == 41
-  dep_times.to_csv(f"./{scenario}_dep_times.csv")
+  # print(deployments)
+  # # need to drop metric as it persists as an index level
+  # means = pd.DataFrame(data={ metric: deployments[[metric]].unstack(level=1).mean().droplevel(0) for metric in ["Hit10Pct", "Hit40Pct", "Hit60Pct", "Hit100Pct"]}) \
+  #   .add_prefix("mean-")
+
+  # stddevs = pd.DataFrame(data={ metric: deployments[[metric]].unstack(level=1).std().droplevel(0) for metric in ["Hit10Pct", "Hit40Pct", "Hit60Pct", "Hit100Pct"]}) \
+  #   .add_prefix("stddev-")
+
+  # dep_times = means.merge(stddevs, left_index=True, right_index=True)
+
+  # print(dep_times)
+  # #assert len(dep_times) == 41
+  # dep_times.to_csv(f"./{scenario}_dep_times.csv")
 
 
 if __name__ == "__main__":
