2b1a0d09830167cc85edcba47328c0440eff4d0d
Dan Birks <danbirks@gmail.com>
2021-11-10 11:34:00 +0000

    add functionality to colate deployment tables for report

--- analysis/mapping/plot_map_results.R
+++ analysis/mapping/plot_map_results.R
@@ -15,6 +15,9 @@ library(viridis)
 options(pillar.sigfig = 10)
 options(digits = 10)
 
+#boolean to turn on or off generating plots - in case you just want to produce output tables 
+generate_plots = FALSE 
+
 setwd("~/Dev/popm")
 sf_gb <- st_read("data/forces.json") #load in the force boundaries
 #alliances <- st_read("data/alliances.geojson")
@@ -46,6 +49,11 @@ for(i in 1:3)
   sizeEvents <- d[i,1] 
   for(j in 2:4)
   {
+    
+    #create a structure to store results for 10,40,60,100% deployments in each of 9 configs to include in report 
+    results_for_report_table <- sf_gb %>% select(name) #first grab force names
+    results_for_report_table <- st_set_geometry(results_for_report_table, NULL) #then drop spatial info
+    
     nEvents <- d[i,j]
     for(deployPCT in c(10,40,60,100))
     {
@@ -101,48 +109,62 @@ for(i in 1:3)
       
       #quickly write out 100% deployment results for table in paper
       tableout <- sf_results %>% select(name,forTable)
-      write_csv(x = tableout, paste0("results/", folder, "/",nEvents,sizeEvents,"_", deployPCT,"PCT.csv"))
+      #drop the sf geometry 
+      tableout <- st_set_geometry(tableout, NULL)
+      
+      # then left join this % deployment results to results_for_report_table
+      results_for_report_table <- left_join(results_for_report_table, tableout, by = c("name" = "name"))
       
       
-      p <- ggplot() +                                                                     # initialise a ggplot object
-        geom_sf(data = sf_results,                                                        # add a simple features (sf) object
-                aes(fill = cut_width(mean, width = 1, center = 0.5)),                   # group mean into equal intervals of 1hr
-                #aes(fill = mean_1_0), 
-                alpha = 0.8,                                                              # add transparency to the fill
-                colour = 'white',                                                         # make polygon boundaries white
-                size = 0.3) +                                                             # adjust width of polygon boundaries
+      #write_csv(x = tableout, paste0("results/", folder, "/",nEvents,sizeEvents,"_", deployPCT,"PCT.csv"))
       
+      #check if we need to generate plots 
+        if (generate_plots == TRUE) 
+        {
+        p <- ggplot() +                                                                     # initialise a ggplot object
+          geom_sf(data = sf_results,                                                        # add a simple features (sf) object
+                  aes(fill = cut_width(mean, width = 1, center = 0.5)),                   # group mean into equal intervals of 1hr
+                  #aes(fill = mean_1_0), 
+                  alpha = 0.8,                                                              # add transparency to the fill
+                  colour = 'white',                                                         # make polygon boundaries white
+                  size = 0.3) +                                                             # adjust width of polygon boundaries
+        
+            
+          scale_fill_brewer(palette = "OrRd",                                               # add palette - in case of more than 9 vals scale_fill_manual(values = getPalette(colourCount),     
+                            name = paste0("Average Hours to \n",deployPCT ,"% Deployment"), # add legend title
+                            drop = TRUE) +
           
-        scale_fill_brewer(palette = "OrRd",                                               # add palette - in case of more than 9 vals scale_fill_manual(values = getPalette(colourCount),     
-                          name = paste0("Average Hours to \n",deployPCT ,"% Deployment"), # add legend title
-                          drop = TRUE) +
-        
-        geom_label_repel(data = sf_results                                                # add the new labels and make sure they don't overlap  
-                         ,aes(x = LAT, y = LONG, label = LABEL)
-                         ,family = 'Avenir',   size=3) +
-        
-        labs(x = NULL, y = NULL) +                                                                               # drop axis titles
-             #title = paste0("Average Hours to " ,deployPCT, "% Deployment (Coefficient of Variation)"),         # add title
-             #subtitle = paste0(nEvents, " ", sizeEvents, " event scenarios - ",deployPCT,"% deployment"),       # add subtitle
-             #caption = "Contains OS data © Crown copyright and database right (2021)") +                        # add copyright
-        theme(panel.background = element_blank(), line = element_blank(), axis.text = element_blank(), axis.title = element_blank()) +
-        coord_sf(datum = NA)
+          geom_label_repel(data = sf_results                                                # add the new labels and make sure they don't overlap  
+                           ,aes(x = LAT, y = LONG, label = LABEL)
+                           ,family = 'Avenir',   size=3) +
+          
+          labs(x = NULL, y = NULL) +                                                                               # drop axis titles
+               #title = paste0("Average Hours to " ,deployPCT, "% Deployment (Coefficient of Variation)"),         # add title
+               #subtitle = paste0(nEvents, " ", sizeEvents, " event scenarios - ",deployPCT,"% deployment"),       # add subtitle
+               #caption = "Contains OS data © Crown copyright and database right (2021)") +                        # add copyright
+          theme(panel.background = element_blank(), line = element_blank(), axis.text = element_blank(), axis.title = element_blank()) +
+          coord_sf(datum = NA)
+  
+          #or plot it without legend 
+          #p <- p + theme(legend.position = "none") 
+          p <- p + theme(legend.position = c(0.89, 0.8))
+          p
+          #save it
+          ggsave(p, filename = paste0("results/", folder, "/REPORT_",nEvents,sizeEvents,"_", deployPCT,"PCT.png"),  width = 20, height = 30, units = "cm")
 
-      #or plot it without legend 
-      #p <- p + theme(legend.position = "none") 
-      p <- p + theme(legend.position = c(0.89, 0.8))
-      p
-      #save it
-      ggsave(p, filename = paste0("results/", folder, "/REPORT_",nEvents,sizeEvents,"_", deployPCT,"PCT.png"),  width = 20, height = 30, units = "cm")
-
+      }
     }
+    #now you've collected deployment numbers at 10,40,60,100% prepare results for report table 
+    colnames(results_for_report_table) <- c("Police Force Area", "10% Deployment", "40% Deployment", "60% Deployment", "100% Deployment")
+    #and write it out 
+    write_csv(x = results_for_report_table, paste0("results/", folder, "/",nEvents,sizeEvents,"_report_table.csv"))
   }
 }
 
 
 
 
-#OPTION 2: FOR 19 SMALL EVENTS WE NEED A 10 COLOR PALLETTE 
+#SPECIAL CASE: FOR 19 SMALL EVENTS WE NEED A 10 COLOR PALLETTE 
 
 #for extending pallette 
 colourCount = 10
